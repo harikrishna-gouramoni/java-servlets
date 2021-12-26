8687591560a830f6f303e0b784044005e2e08223
harik <harik@DESKTOP-J0D6PUM>
2021-12-26 15:50:49 +0530

    Search method changes

--- AdharManagement/src/main/java/com/adhar/service/SearchService.java
+++ AdharManagement/src/main/java/com/adhar/service/SearchService.java
@@ -4,6 +4,11 @@ import com.adhar.bean.Customer;
 
 public class SearchService {
 
+	/**
+	 * Need to update this service with API call
+	 * @param aadharNumber
+	 * @return
+	 */
 	public static Customer getAadharSerachResult(String aadharNumber) {
 		
 		//call to DAO and get the Customer information
